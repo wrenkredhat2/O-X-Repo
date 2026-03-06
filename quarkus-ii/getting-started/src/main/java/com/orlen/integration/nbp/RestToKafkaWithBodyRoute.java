c1b5120c235ecb950b42cc9d3f5c33cf980bf895
Wolfgang Renk <wrenk@redhat.com>
2026-03-06 16:36:47 +0100

    Keykloak 80percent

--- quarkus-ii/getting-started/src/main/java/com/orlen/integration/nbp/RestToKafkaWithBodyRoute.java
+++ quarkus-ii/getting-started/src/main/java/com/orlen/integration/nbp/RestToKafkaWithBodyRoute.java
@@ -1,3 +1,5 @@
+package com.orlen.integration.nbp;
+
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.Exchange;
 import org.apache.camel.model.dataformat.JsonLibrary;
@@ -19,22 +21,37 @@ public class RestToKafkaWithBodyRoute extends RouteBuilder {
             // 1. Prepare for the outgoing call
             // We set the HTTP method for the external service
             .setHeader(Exchange.HTTP_METHOD, constant("GET"))
-//            .setHeader(Exchange.CONTENT_TYPE, constant("application/json"))
-            .setHeader(Exchange.CONTENT_TYPE, constant("application/xml"))
+            .setHeader(Exchange.CONTENT_TYPE, constant("application/json"))
+//            .setHeader(Exchange.CONTENT_TYPE, constant("application/xml"))
             .removeHeader(Exchange.HTTP_PATH)
 
             // 2. Call the external service
             // The body received in the 'rest' call is automatically sent as the payload
             .to("https://api.nbp.pl/api/exchangerates/tables/A?bridgeEndpoint=true")
-            
+            // https://www.youtube.com/watch?v=Hj6iZgTsXmo
+            //onExcepetion
+            //ErrorHandler
+            //filter
+            // Choice
+             .log ("Hello From Route -- after nbp")
+             .log("Response Body: ${body}")
+             .convertBodyTo(String.class)
             // 3. Transform the service's response to JSON (if it isn't already)
             .marshal().json(JsonLibrary.Jackson)
-            
+             .log ("Hello From Route -- after nbp")
+     
+             
             // 4. Send the result to Kafka
 //            .to("kafka:processed-data?brokers={{kafka.bootstrap.servers}}" +
-//                "&securityProtocol={{kafka.security.protocol}}" +
-//                "&sslTruststoreLocation={{kafka.ssl.truststore.location}}" +
-//                "&sslTruststorePassword={{kafka.ssl.truststore.password}}")
+//                  "&securityProtocol={{kafka.security.protocol}}" +
+//                  "&sslTruststoreLocation={{kafka.ssl.truststore.location}}" +
+//                  "&sslTruststorePassword={{kafka.ssl.truststore.password}}");
+
+// https://orl-clust-kafka-listener1-bootstrap-kafka-streams.apps.cluster-492gk.492gk.sandbox2076.opentlc.com
+            .to("kafka:nbp-data?brokers=orl-clust-kafka-listener1-bootstrap-kafka-streams.apps.cluster-492gk.492gk.sandbox2076.opentlc.com:443" +
+                  "&securityProtocol=SSL" +
+                  "&sslTruststoreLocation=/home/wrenk/CLIENTS/ORLEN/quarkus-ii/getting-started/truststore.jks" +
+                  "&sslTruststorePassword=123456")
             
             // 5. Optional: Return a success message to the original caller
             .setBody(constant("{\"status\": \"Message processed and sent to Kafka\"}"));
---
+++ quarkus-ii/getting-started/src/main/resources/etc/createTruststore.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Configuration - Update these to match your environment
+CLUSTER_NAME="orl-clust"       # Your Kafka cluster name
+NAMESPACE="kafka-streams"       # The namespace where Kafka is running
+TS_PASSWORD="123456"          # Password for your new truststore
+TS_FILE="truststore.jks"        # Output filename
+
+echo "--- Extracting Cluster CA Certificate ---"
+# 1. Download the public CA certificate from the OpenShift Secret
+oc get secret "${CLUSTER_NAME}-cluster-ca-cert" \
+  -n "$NAMESPACE" \
+  -o jsonpath='{.data.ca\.crt}' | base64 --decode > ca.crt
+
+echo "--- Creating JKS Truststore ---"
+# 2. Delete the old truststore if it exists
+rm -f "$TS_FILE"
+
+# 3. Import the certificate into a new JKS truststore
+echo keytool -importcert \
+  -alias strimzi-cluster-ca \
+  -file ca.crt \
+  -keystore "$TS_FILE" \
+  -storepass "$TS_PASSWORD" \
+  -noprompt
+
+echo "--- Cleanup ---"
+rm ca.crt
+
+echo "Success! Your truststore is ready at: $(pwd)/$TS_FILE"
